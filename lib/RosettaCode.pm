d475e2dbb493c2208ffa0ad563ffb3d77aac8466
Ingy döt Net <ingy@ingy.net>
2014-07-26 11:58:11 -0700

    0.0.6

--- lib/RosettaCode.pm
+++ lib/RosettaCode.pm
@@ -1,7 +1,5 @@
-use v5.14;
 package RosettaCode;
-
-our $VERSION = '0.0.5';
+our $VERSION = '0.0.6';
 
 use utf8;
 use MediaWiki::Bot;
@@ -334,49 +332,3 @@ sub parse_fail {
 }
 
 1;
-
-=encoding utf8
-
-=head1 NAME
-
-RosettaCode - An Application to interface with http://rosettacode.org
-
-=head1 SYNOPSIS
-
-From the command line:
-
-    > rosettacode help
-    > git clone git://github.com/acmeism/RosettaCodeData.git
-    > cd RosettaCodeData
-    > rosettacode sync .
-
-=head1 DESCRIPTION
-
-RosettaCode.org is a fantastic wiki that contains ~ 650 programming tasks,
-each implemented in up to ~ 500 programming languages.
-
-This tool aims to make it easier for programmers to obtain and try the various
-code samples.
-
-At this point, the main function is to extract the code examples and put them
-into a git repository on GitHub. You probably don't need to use this tool
-yourself. You can just get the repository here:
-
-    git clone git://github.com/acmeism/RosettaCodeData.git
-
-This tool will just update all the files in that repository.
-
-=head1 AUTHOR
-
-Ingy döt Net <ingy@cpan.org>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (c) 2013. Ingy döt Net.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-See http://www.perl.com/perl/misc/Artistic.html
-
-=cut
